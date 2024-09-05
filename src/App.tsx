3d18b445bb824e295790628dce2036fc1deafc67
GOODERESTER <66927822+GOODERESTER@users.noreply.github.com>
2024-09-04 20:49:36 -0400

    added upload files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import { useEffect, useState } from "react";
+import React from 'react';
+import { uploadData } from 'aws-amplify/storage';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { StorageManager } from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react/styles.css';
+
+
+export const DefaultStorageManagerExample = () => {
+  return (
+    <StorageManager
+      acceptedFileTypes={['image/*']}
+      path="public/"
+      maxFileCount={1}
+      isResumable
+    />
+  );
+};
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]); 
+  const [file, setFile] = useState<File | null>(null);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+    const handleChange = (event: any) => {
+        setFile(event.target.files[0]);
+    };
 
     
   function deleteTodo(id: string) {
@@ -25,31 +40,53 @@ function App() {
   }
 
   return (
+    <div> 
+      <Authenticator>
         
-    <Authenticator>
-      
-      {({ signOut, user }) => (
-      <main>
-        <h1>{user?.signInDetails?.loginId}'s todos</h1>
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-          {todos.map((todo) => (
-            <li 
-            onClick={() => deleteTodo(todo.id)}
-            key={todo.id}>{todo.content}</li>
-          ))}
-        </ul>
-        <div>
-          🥳 App successfully hosted. Try creating a new todo.
-          <br />
-          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-            Review next step of this tutorial.
-          </a>
+        {({ signOut, user }) => (
+        <main>
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo) => (
+              <li 
+              onClick={() => deleteTodo(todo.id)}
+              key={todo.id}>{todo.content}</li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+              Review next step of this tutorial.
+            </a>
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+      </Authenticator>
+      <div>
+            <input type="file" onChange={handleChange} />
+            <button
+                onClick={() => {
+                  try {
+                    if (!file) {
+                      throw new Error("No file selected");
+                    }
+                    uploadData({
+                      path: `clothing-photos/${file.name}`,
+                      data: file,
+                    });
+                  } catch (error) {
+                    console.error(error);
+                  }
+                    
+                }}
+            >
+                Upload
+            </button>
         </div>
-        <button onClick={signOut}>Sign out</button>
-      </main>
-    )}
-    </Authenticator>
+    </div>
   );
 }
 
